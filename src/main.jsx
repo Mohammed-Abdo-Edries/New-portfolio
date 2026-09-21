20ad2de18ff81945d8c9a9265428180e62d2d780
Mohammed-Abdo-Edries <mohammed.abdo1916@gmail.com>
2026-09-21 22:13:36 +0200

    added dark mode1

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { ThemeProvider  } from "./context/ThemeContext.jsx";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <ThemeProvider>
+  <ThemeProvider >
     <App />
-  </ThemeProvider>
-  </React.StrictMode>,
+  </ThemeProvider >
+  </React.StrictMode>
 )
